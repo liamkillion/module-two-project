0ea4d8803f5f427afedcbde757bf46e8f0e0df32
github username <github email address>
2017-11-13 15:02:04 -0500

    new colum pic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113174343) do
+ActiveRecord::Schema.define(version: 20171113190503) do
 
   create_table "article_outfits", force: :cascade do |t|
     t.integer "outfit_id"
@@ -27,6 +27,10 @@ ActiveRecord::Schema.define(version: 20171113174343) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "outfits", force: :cascade do |t|
