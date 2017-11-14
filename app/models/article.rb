7b5b3574aa3186d41810427d02cab64a20edcdb4
Liam Killion <liam.killion@gmail.com>
2017-11-14 16:11:32 -0500

    added in user/articles hasmany/belongsto relationship

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   has_many :outfits, through: :article_outfits
   # belongs_to :user
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
